c38f3e58507ed24c737d22edd942de8610ad1b52
Vincent Marquez <vincent.marquez@gmail.com>
2016-07-07 00:22:29 -0700

    holy cow profunctors are magic

--- base/src/main/scala/optics/Lens.scala
+++ base/src/main/scala/optics/Lens.scala
@@ -18,17 +18,24 @@ trait Lens[S, T, A, B] { self =>
     val p = Strong[Function1]
     stab[Function1](p)((a: A) => b)(s)
   }
+
+  def compose[C, D](lens: Lens[A, B, C, D]): Lens[S, T, C, D] = new Lens[S, T, C, D] {
+    override def stab[P[_,_]: Strong]: P[C, D] => P[S, T] = (pcd: P[C, D]) => {
+      val pab = lens.stab(Strong[P])(pcd)
+      self.stab(Strong[P])(pab)
+    }
+  }
 }
 
 object Lens {
   
   def apply[S, T, A, B](get: S => A, set: (B, S) => T): Lens[S, T, A, B] = new Lens[S, T, A, B] {
     override def stab[P[_, _]: Strong]: P[A, B] => P[S, T] = (pab: P[A, B]) => { 
-      val tupled = Strong[P].first[A, B, S](pab)
-      Profunctor[P].dimap(tupled)((s: S) => (get(s),s ))(t => set(t._1, t._2))
+      val t = Strong[P].first[A, B, S](pab)
+      Profunctor[P].dimap(t)((s: S) => (get(s),s ))(t => set(t._1, t._2))
     }
   }
   case class User(name: String, age: Int)
- 
+
 }
 
